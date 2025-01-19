8e565ef4be58110af96975a7d5555a5751a6b283
Hesam Ghadimi <hess.gha@gmail.com>
2025-01-19 17:01:53 +0100

    feat : full Gourand on pi

--- Common/Assets/Shaders/Pi/LitFragment.glsl
+++ Common/Assets/Shaders/Pi/LitFragment.glsl
@@ -1,134 +1,15 @@
+// Fragment Shader
 #version 310 es
-
-// Pi
-
-#define MAX_LIGHTS_COUNT 4
-#define POINT_LIGHT 0
-#define DIR_LIGHT 1
-#define SPOT_LIGHT 2
-
 precision highp float;
 
-// in
 in vec2 ioTexCoord;
-in vec3 ioNormal;
-in vec3 ioFragPos;
-in mat4 ioModel;
+in vec3 ioLightingColor;
 
-// out
 out vec4 FragColor;
 
-// uniform
 uniform sampler2D texture_diffuse1;
-uniform sampler2D texture_specular1;
 
-struct Global
-{
-    float ambientStrength;
-    vec3 ambientColor;
-    vec3 viewPos;
-    int lightsCount;
-};
-uniform Global global;
-
-struct Light
-{
-    int type;
-
-    vec3 diffuseColor;
-
-    vec3 pos;
-    vec3 dir;
-
-    // PointLight
-    float attConst;
-    float attLin;
-    float attQuad;
-
-    // SpotLight
-    float cutOff;
-};
-uniform Light lights[MAX_LIGHTS_COUNT];
-
-vec3 CalcPointLight(Light light, vec3 viewDir);
-vec3 CalcDirLight(Light light, vec3 viewDir);
-vec3 CalcSpotLight(Light light, vec3 viewDir);
-
-void main()
-{
-    vec3 sum = vec3(0.0);
-    vec3 viewDir = normalize(global.viewPos - ioFragPos);
+void main() {
     vec4 textureColor = texture(texture_diffuse1, ioTexCoord);
-
-    vec3 ambientColor = global.ambientStrength * global.ambientColor;
-    sum += ambientColor;
-
-    for(int i = 0; i < global.lightsCount; i++)
-    {
-        Light light = lights[i];
-        if(light.type == POINT_LIGHT)
-        {
-            sum += CalcPointLight(light, viewDir);
-        }
-        else if(light.type == DIR_LIGHT)
-        {
-            sum += CalcDirLight(light, viewDir);
-        }
-        else if(light.type == SPOT_LIGHT)
-        {
-            sum += CalcSpotLight(light, viewDir);
-        }
-    }
-
-    vec3 result = sum * vec3(textureColor);
-
-    FragColor = vec4(result, 1.0);
+    FragColor = vec4(ioLightingColor * vec3(textureColor), 1.0);
 }
-
-vec3 CalcDirLight(Light light, vec3 viewDir)
-{
-    vec3 sum = vec3(0.0);
-
-    vec3 lightDir = normalize(-light.dir);
-    float angleDifference = max(dot(ioNormal, lightDir), 0.0);
-    vec3 diffuse = angleDifference * light.diffuseColor;
-    sum += diffuse;
-
-    return sum;
-}
-
-vec3 CalcPointLight(Light light, vec3 viewDir)
-{
-    vec3 sum = vec3(0.0);
-
-    float distance = length(light.pos - ioFragPos);
-    float attenuation = 1.0 / (light.attConst + light.attLin * distance + light.attQuad * (distance * distance));
-
-    vec3 lightDir = normalize(light.pos - ioFragPos);
-    float angleDifference = max(dot(ioNormal, lightDir), 0.0);
-    vec3 diffuse = angleDifference * light.diffuseColor;
-    sum += diffuse * attenuation;
-
-    return sum;
-}
-
-vec3 CalcSpotLight(Light light, vec3 viewDir)
-{
-    vec3 sum = vec3(0.0);
-
-    vec3 lightDir = normalize(light.pos - ioFragPos);
-    float theta = dot(lightDir, normalize(-light.dir));
-
-    if(theta > light.cutOff)
-    {
-        float distance = length(light.pos - ioFragPos);
-        float attenuation = 1.0 / (light.attConst + light.attLin * distance + light.attQuad * (distance * distance));
-
-        float angleDifference = max(dot(ioNormal, lightDir), 0.0);
-        vec3 diffuse = angleDifference * light.diffuseColor;
-        sum += diffuse * attenuation;
-    }
-
-    return sum;
-}
-
--- Common/Assets/Shaders/Pi/LitVertex.glsl
+++ Common/Assets/Shaders/Pi/LitVertex.glsl
@@ -1,7 +1,8 @@
 #version 310 es
-
-// Pi
-
+#define MAX_LIGHTS_COUNT 5
+#define POINT_LIGHT 0
+#define DIR_LIGHT 1
+#define SPOT_LIGHT 2
 precision highp float;
 
 // in
@@ -11,20 +12,81 @@ layout (location = 2) in vec2 iTexCoord;
 
 // out
 out vec2 ioTexCoord;
-out vec3 ioNormal;
-out vec3 ioFragPos;
-out mat4 ioModel;
+out vec3 ioLightingColor;
 
 // uniform
 uniform mat4 uMVP;
 uniform mat4 uModel;
 uniform mat4 uModelNormal;
+uniform sampler2D texture_diffuse1;
+uniform sampler2D texture_specular1;
 
-void main()
-{
-	gl_Position = uMVP * vec4(iPos, 1.0);
-	ioFragPos = vec3(uModel * vec4(iPos, 1.0));
-	ioModel = uModel;
-	ioTexCoord = iTexCoord;
-	ioNormal = normalize(mat3(uModelNormal) * iNormal);
+struct Global {
+    float ambientStrength;
+    vec3 ambientColor;
+    vec3 viewPos;
+    int lightsCount;
+};
+uniform Global global;
+
+struct Light {
+    int type;
+    vec3 diffuseColor;
+    vec3 pos;
+    vec3 dir;
+    float attConst;
+    float attLin;
+    float attQuad;
+    float cutOff;
+};
+uniform Light lights[MAX_LIGHTS_COUNT];
+
+vec3 CalcDirLight(Light light, vec3 normal, vec3 fragPos) {
+    vec3 lightDir = normalize(-light.dir);
+    float angleDifference = max(dot(normal, lightDir), 0.0);
+    return angleDifference * light.diffuseColor;
+}
+
+vec3 CalcPointLight(Light light, vec3 normal, vec3 fragPos) {
+    float distance = length(light.pos - fragPos);
+    float attenuation = 1.0 / (light.attConst + light.attLin * distance + light.attQuad * (distance * distance));
+    vec3 lightDir = normalize(light.pos - fragPos);
+    float angleDifference = max(dot(normal, lightDir), 0.0);
+    return angleDifference * light.diffuseColor * attenuation;
+}
+
+vec3 CalcSpotLight(Light light, vec3 normal, vec3 fragPos) {
+    vec3 lightDir = normalize(light.pos - fragPos);
+    float theta = dot(lightDir, normalize(-light.dir));
+    if(theta > light.cutOff) {
+        float distance = length(light.pos - fragPos);
+        float attenuation = 1.0 / (light.attConst + light.attLin * distance + light.attQuad * (distance * distance));
+        float angleDifference = max(dot(normal, lightDir), 0.0);
+        return angleDifference * light.diffuseColor * attenuation;
+    }
+    return vec3(0.0);
+}
+
+void main() {
+    vec3 fragPos = vec3(uModel * vec4(iPos, 1.0));
+    vec3 normal = normalize(mat3(uModelNormal) * iNormal);
+    
+    vec3 sum = global.ambientStrength * global.ambientColor;
+    
+    for(int i = 0; i < global.lightsCount; i++) {
+        Light light = lights[i];
+        if(light.type == POINT_LIGHT) {
+            sum += CalcPointLight(light, normal, fragPos);
+        }
+        else if(light.type == DIR_LIGHT) {
+            sum += CalcDirLight(light, normal, fragPos);
+        }
+        else if(light.type == SPOT_LIGHT) {
+            sum += CalcSpotLight(light, normal, fragPos);
+        }
+    }
+    
+    ioLightingColor = sum;
+    ioTexCoord = iTexCoord;
+    gl_Position = uMVP * vec4(iPos, 1.0);
 }
